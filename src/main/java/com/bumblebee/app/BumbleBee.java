2796fbbceb2bcb627efdfb05a06e111ed2cff01a
oOo <Barala>
2016-11-21 05:46:20 +0800

    issue resolved

--- src/main/java/com/bumblebee/app/BumbleBee.java
+++ src/main/java/com/bumblebee/app/BumbleBee.java
@@ -10,7 +10,7 @@ public class BumbleBee
 {
 	private static String dirPath = "/home/barala/Desktop/apache-cassandra-2.1.13/data/data/vnodetesting/table1-b72ed441807411e695e66f65bc3254e2";
 	private static String hostName = "172.26.147.166";
-	private static boolean debugMode = true;
+	private static boolean debugMode = false;
 	private static String schemaPath = "/home/barala/Bumblebee/bumblebee/src/test/resources/schema/schema.cql";
 	private static String outputDir= "/home/barala/Bumblebee";
 	
@@ -28,9 +28,9 @@ public class BumbleBee
     	try {
         	ProcessSStable processSStable = new ProcessSStable(clientInfo.getClient(),clientInfo.getDifrectory(),clientInfo.getOutputDirPath(),clientInfo.getSchemaPath());
 			processSStable.runner();
+			System.out.println( "Processed without any glitch!!" );
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        System.out.println( "Processed without any glitch!!" );
     }
 }
